c06c8948fb33cdbd1adc477dc3a4895c36501803
Alex Wilson <a@ax.gy>
2017-01-29 22:53:24 +0000

    Organising layout a little

--- macos/install.sh
+++ macos/install.sh
@@ -3,7 +3,6 @@
 ## Dark mode
 osascript -e 'tell application "System Events" to tell appearance preferences to set properties to {dark mode:true}'
 
-
 ## Tap to click
 defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
